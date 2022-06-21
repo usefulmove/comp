182e94643a5565fb9f0fc2ec6e33f0e19bc0c104
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-06-21 02:34:26 -0700

    completed implementation and testing of remaining commands

--- src/comp.rs
+++ src/comp.rs
@@ -65,13 +65,17 @@ fn processnode(cs: &mut CompositeStack, cmdval: &str) {
     "dup"    => c_dup(cs),  // duplicate
     "swap"   => c_swap(cs), // swap x and y
     "cls"    => c_cls(cs),  // clear stack
+    "clr"    => c_cls(cs),  // clear stack
     "roll"   => c_roll(cs), // roll stack
     // memory usage
     "sa"     => c_store_a(cs), // store (pop value off stack and store)
+    ".a"     => c_store_a(cs), // store (pop value off stack and store)
     "a"      => c_push_a(cs),  // retrieve (push stored value onto the stack)
     "sb"     => c_store_b(cs), // store
+    ".b"     => c_store_b(cs), // store
     "b"      => c_push_b(cs),  // retrieve
     "sc"     => c_store_c(cs), // store
+    ".c"     => c_store_c(cs), // store
     "c"      => c_push_c(cs),  // retrieve
     // math operations
     "+"      => c_add(cs),
@@ -82,12 +86,31 @@ fn processnode(cs: &mut CompositeStack, cmdval: &str) {
     "/"      => c_div(cs),
     "chs"    => c_chs(cs),
     "abs"    => c_abs(cs),
+    "round"  => c_round(cs),
+    "int"    => c_round(cs),
     "inv"    => c_inv(cs),
     "sqrt"   => c_sqrt(cs),
     "throot" => c_throot(cs),
+    "proot"  => c_proot(cs),
     "^"      => c_exp(cs),
+    "exp"    => c_exp(cs),
     "%"      => c_mod(cs),
+    "mod"      => c_mod(cs),
     "!"      => c_fact(cs),
+    "gcd"    => c_gcd(cs),
+    "pi"     => c_pi(cs),
+    "e"      => c_euler(cs),
+    "dtor"   => c_dtor(cs),
+    "rtod"   => c_rtod(cs),
+    "sin"    => c_sin(cs),
+    "asin"   => c_asin(cs),
+    "cos"    => c_cos(cs),
+    "acos"   => c_acos(cs),
+    "tan"    => c_tan(cs),
+    "atan"   => c_atan(cs),
+    "log"    => c_log10(cs),
+    "log10"  => c_log10(cs),
+    "ln"     => c_ln(cs),
     _ => cs.stack.push(cmdval.parse::<f64>().unwrap()), // push value onto stack
   }
 }
@@ -184,14 +207,19 @@ fn c_div(cs: &mut CompositeStack) {
   cs.stack[end-1] /= cs.stack.pop().unwrap();
 }
 
+fn c_chs(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = -1.0 * cs.stack[end];
+}
+
 fn c_abs(cs: &mut CompositeStack) {
   let end: usize = cs.stack.len() - 1;
   cs.stack[end] = f64::abs(cs.stack[end]);
 }
 
-fn c_chs(cs: &mut CompositeStack) {
+fn c_round(cs: &mut CompositeStack) {
   let end: usize = cs.stack.len() - 1;
-  cs.stack[end] = -1.0 * cs.stack[end];
+  cs.stack[end] = cs.stack[end].round();
 }
 
 fn c_inv(cs: &mut CompositeStack) {
@@ -210,6 +238,24 @@ fn c_throot(cs: &mut CompositeStack) {
   cs.stack[end-1] = cs.stack[end-1].powf(1.0/o);
 }
 
+fn c_proot(cs: &mut CompositeStack) {
+  let c: f64 = cs.stack.pop().unwrap();
+  let b: f64 = cs.stack.pop().unwrap();
+  let a: f64 = cs.stack.pop().unwrap();
+
+  if (b*b - 4.0*a*c) < 0.0 {
+    cs.stack.push(-1.0*b/(2.0*a)); // root1 real
+    cs.stack.push(f64::sqrt(4.0*a*c-b*b)/(2.0*a)); // root1 imag
+    cs.stack.push(-1.0*b/(2.0*a)); // root2 real
+    cs.stack.push(-1.0*f64::sqrt(4.0*a*c-b*b)/(2.0*a)); // root2 imag
+  } else {
+    cs.stack.push(-1.0*b+f64::sqrt(b*b-4.0*a*c)/(2.0*a)); // root1 real
+    cs.stack.push(0.0); // root1 imag
+    cs.stack.push(-1.0*b-f64::sqrt(b*b-4.0*a*c)/(2.0*a)); // root2 real
+    cs.stack.push(0.0); // root2 imag
+  }
+}
+
 fn c_exp(cs: &mut CompositeStack) {
   let end: usize = cs.stack.len() - 1;
   let o: f64 = cs.stack.pop().unwrap();
@@ -225,6 +271,79 @@ fn c_mod(cs: &mut CompositeStack) {
 fn c_fact(cs: &mut CompositeStack) {
   let end: usize = cs.stack.len() - 1;
   cs.stack[end] = factorial(cs.stack[end] as u64) as f64;
+}
+
+fn c_gcd(cs: &mut CompositeStack) {
+  let a: u64 = cs.stack.pop().unwrap() as u64;
+  let b: u64 = cs.stack.pop().unwrap() as u64;
+  let g: f64 = gcd(a,b) as f64;
+  cs.stack.push(g);
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+  if b != 0 {
+    return gcd(b, a % b)
+  } else {
+    return a
+  }
+}
+
+fn c_pi(cs: &mut CompositeStack) {
+  cs.stack.push(std::f64::consts::PI);
+}
+
+fn c_euler(cs: &mut CompositeStack) {
+  cs.stack.push(std::f64::consts::E);
+}
+
+fn c_dtor(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] *= std::f64::consts::PI / 180.0;
+}
+
+fn c_rtod(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] *= 180.0 / std::f64::consts::PI;
+}
+
+fn c_sin(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].sin();
+}
+
+fn c_asin(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].asin();
+}
+
+fn c_cos(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].cos();
+}
+
+fn c_acos(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].acos();
+}
+
+fn c_tan(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].tan();
+}
+
+fn c_atan(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].atan();
+}
+
+fn c_log10(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].log10();
+}
+
+fn c_ln(cs: &mut CompositeStack) {
+  let end: usize = cs.stack.len() - 1;
+  cs.stack[end] = cs.stack[end].ln();
 }
 
 fn factorial(n: u64) -> u64 {

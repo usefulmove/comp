dc8f3de917277ced263946331550c6e01265b643
Duane <duane.edmonds@gmail.com>
2022-07-04 03:00:04 -0700

    signficant refactor to replace process_node method match structure with hash map; manual tests complete; unit tests need to be updated

--- src/comp.rs
+++ src/comp.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 use std::num::ParseFloatError;
 use std::path::Path;
 use std::path::Display;
+use std::collections::HashMap;
 
 const COMP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -45,15 +46,9 @@ fn main() {
 
   let mut args: Vec<String> = env::args().collect();
 
-  // create computation processor with stack, memory slots and an operations list
-  let mut proc = Processor {
-    stack: Vec::new(),
-    mem_a: 0.0,
-    mem_b: 0.0,
-    mem_c: 0.0,
-    ops: Vec::new(),
-    fns: Vec::new(),
-  };
+  // create computation processor
+  let mut proc = Processor::new();
+
 
   if args.len() <= 1 {
     args.push("help".to_string());
@@ -142,6 +137,7 @@ struct Processor {
   mem_c: f64,
   ops: Vec<String>,
   fns: Vec<Function>,
+  dict: HashMap<String, fn(&mut Processor)>,
 }
 
 struct Function {
@@ -150,6 +146,23 @@ struct Function {
 }
 
 impl Processor {
+  // constructor
+  fn new() -> Processor {
+    let mut p = Processor {
+      stack: Vec::new(),
+      mem_a: 0.0,
+      mem_b: 0.0,
+      mem_c: 0.0,
+      ops: Vec::new(),
+      fns: Vec::new(),
+      dict: HashMap::new(),
+    };
+
+    p.construct();
+
+    p
+  }
+
   fn process_ops(&mut self) {
     while !self.ops.is_empty() {
       let operation: String = self.ops.remove(0); // pop first operation
@@ -157,85 +170,94 @@ impl Processor {
     }
   }
 
+  fn add_command(&mut self, name: &str, func: fn(&mut Processor)) {
+    self.dict.insert(name.to_string(), func);
+  }
+
+  fn construct(&mut self) {
+    // stack manipulation
+    self.add_command("drop",   Processor::c_drop);      // drop
+    self.add_command("dup",    Processor::c_dup);       // duplicate
+    self.add_command("swap",   Processor::c_swap);      // swap x and y
+    self.add_command("cls",    Processor::c_cls);       // clear stack
+    self.add_command("clr",    Processor::c_cls);       // clear stack
+    self.add_command("roll",   Processor::c_roll);      // roll stack
+    // memory usage
+    self.add_command("sa",     Processor::c_store_a);   // store (pop value off stack and store)
+    self.add_command(".a",     Processor::c_store_a);   // store (pop value off stack and store)
+    self.add_command("a",      Processor::c_push_a);    // retrieve (push stored value onto the stack)
+    self.add_command("sb",     Processor::c_store_b);   // store
+    self.add_command(".b",     Processor::c_store_b);   // store
+    self.add_command("b",      Processor::c_push_b);    // retrieve
+    self.add_command("sc",     Processor::c_store_c);   // store
+    self.add_command(".c",     Processor::c_store_c);   // store
+    self.add_command("c",      Processor::c_push_c);    // retrieve
+    // math operations
+    self.add_command("+",      Processor::c_add);       // add
+    self.add_command("+_",     Processor::c_add_all);   // add all
+    self.add_command("-",      Processor::c_sub);       // subtract
+    self.add_command("x",      Processor::c_mult);      // multiply
+    self.add_command("x_",     Processor::c_mult_all);  // multiply all
+    self.add_command("/",      Processor::c_div);       // divide
+    self.add_command("chs",    Processor::c_chs);       // change sign
+    self.add_command("abs",    Processor::c_abs);       // absolute value
+    self.add_command("round",  Processor::c_round);     // round
+    self.add_command("int",    Processor::c_round);
+    self.add_command("inv",    Processor::c_inv);       // invert (1/x)
+    self.add_command("sqrt",   Processor::c_sqrt);      // square root
+    self.add_command("throot", Processor::c_throot);    // nth root
+    self.add_command("proot",  Processor::c_proot);     // find principal roots
+    self.add_command("^",      Processor::c_exp);       // exponenation
+    self.add_command("exp",    Processor::c_exp);
+    self.add_command("%",      Processor::c_mod);       // modulus
+    self.add_command("mod",    Processor::c_mod);
+    self.add_command("!",      Processor::c_fact);      // factorial
+    self.add_command("gcd",    Processor::c_gcd);       // greatest common divisor
+    self.add_command("pi",     Processor::c_pi);        // pi
+    self.add_command("e",      Processor::c_euler);     // Euler's constant
+    self.add_command("dtor",   Processor::c_dtor);      // degrees to radians
+    self.add_command("rtod",   Processor::c_rtod);      // radians to degrees
+    self.add_command("sin",    Processor::c_sin);       // sine
+    self.add_command("asin",   Processor::c_asin);      // arcsine
+    self.add_command("cos",    Processor::c_cos);       // cosine
+    self.add_command("acos",   Processor::c_acos);      // arccosine
+    self.add_command("tan",    Processor::c_tan);       // tangent
+    self.add_command("atan",   Processor::c_atan);      // arctangent
+    self.add_command("log",    Processor::c_log10);     // log (base 10)
+    self.add_command("log10",  Processor::c_log10);
+    self.add_command("ln",     Processor::c_ln);        // natural log
+    // control flow
+    self.add_command("fn",     Processor::c_fn);        // function definition
+  }
+
   fn process_node(&mut self, op: &str) {
-    match op {
-      // stack manipulation
-      "drop"   => self.c_drop(),      // drop
-      "dup"    => self.c_dup(),       // duplicate
-      "swap"   => self.c_swap(),      // swap x and y
-      "cls"    => self.c_cls(),       // clear stack
-      "clr"    => self.c_cls(),       // clear stack
-      "roll"   => self.c_roll(),      // roll stack
-      // memory usage
-      "sa"     => self.c_store_a(),   // store (pop value off stack and store)
-      ".a"     => self.c_store_a(),   // store (pop value off stack and store)
-      "a"      => self.c_push_a(),    // retrieve (push stored value onto the stack)
-      "sb"     => self.c_store_b(),   // store
-      ".b"     => self.c_store_b(),   // store
-      "b"      => self.c_push_b(),    // retrieve
-      "sc"     => self.c_store_c(),   // store
-      ".c"     => self.c_store_c(),   // store
-      "c"      => self.c_push_c(),    // retrieve
-      // math operations
-      "+"      => self.c_add(),       // add
-      "+_"     => self.c_add_all(),   // add all
-      "-"      => self.c_sub(),       // subtract
-      "x"      => self.c_mult(),      // multiply
-      "x_"     => self.c_mult_all(),  // multiply all
-      "/"      => self.c_div(),       // divide
-      "chs"    => self.c_chs(),       // change sign
-      "abs"    => self.c_abs(),       // absolute value
-      "round"  => self.c_round(),     // round
-      "int"    => self.c_round(),
-      "inv"    => self.c_inv(),       // invert (1/x)
-      "sqrt"   => self.c_sqrt(),      // square root
-      "throot" => self.c_throot(),    // nth root
-      "proot"  => self.c_proot(),     // find principal roots
-      "^"      => self.c_exp(),       // exponenation
-      "exp"    => self.c_exp(),
-      "%"      => self.c_mod(),       // modulus
-      "mod"    => self.c_mod(),
-      "!"      => self.c_fact(),      // factorial
-      "gcd"    => self.c_gcd(),       // greatest common divisor
-      "pi"     => self.c_pi(),        // pi
-      "e"      => self.c_euler(),     // Euler's constant
-      "dtor"   => self.c_dtor(),      // degrees to radians
-      "rtod"   => self.c_rtod(),      // radians to degrees
-      "sin"    => self.c_sin(),       // sine
-      "asin"   => self.c_asin(),      // arcsine
-      "cos"    => self.c_cos(),       // cosine
-      "acos"   => self.c_acos(),      // arccosine
-      "tan"    => self.c_tan(),       // tangent
-      "atan"   => self.c_atan(),      // arctangent
-      "log"    => self.c_log10(),     // log (base 10)
-      "log10"  => self.c_log10(),
-      "ln"     => self.c_ln(),        // natural log
-      // control flow
-      "fn"     => self.c_fn(),        // function definition
-      _ => { let ind: i32 = self.is_user_function(op);
-             if ind != -1 { // user-defined function?
-               // copy user function ops (fops) into main ops
-               for i in (0..self.fns[ind as usize].fops.len()).rev() {
-                 let fop: String = self.fns[ind as usize].fops[i].clone();
-                 self.ops.insert(0, fop);
-               }
+    if self.dict.contains_key(op) {
+      let f = self.dict[op];
+      f(self);
+    } else {
+      let ind: i32 = self.is_user_function(op);
+      if ind != -1 { // user-defined function?
+        // copy user function ops (fops) into main ops
+        for i in (0..self.fns[ind as usize].fops.len()).rev() {
+          let fop: String = self.fns[ind as usize].fops[i].clone();
+          self.ops.insert(0, fop);
+        }
 
-             } else {
-               let res: Result<f64, ParseFloatError> = self.parse_value(op);
-               
-               let val = match res {
-                 Ok(val) => val,
-                 Err(_error) => {
-                   eprintln!("error: comp interpreter was passed an unknown \
-                              expression: [{op}] is not a recognized operation \
-                              or value");
-                   std::process::exit(99);
-                 },
-               };
+      } else {
+        let res: Result<f64, ParseFloatError> = self.parse_value(op);
+        
+        let val = match res {
+          Ok(val) => val,
+          Err(_error) => {
+            eprintln!("error: comp interpreter was passed an unknown \
+                       expression: [{op}] is not a recognized operation \
+                       or value");
+            std::process::exit(99);
+          },
+        };
 
-               self.stack.push(val);
-             }
-           },
+        self.stack.push(val);
+      }
     }
   }
 
@@ -583,6 +605,7 @@ mod comp_tests {
       mem_c: -123.45,
       ops: Vec::new(),
       fns: Vec::new(),
+      dict: HashMap::new(),
     };
 
     test_proc.stack.push(1.0);
@@ -623,13 +646,14 @@ mod comp_tests {
   #[test]
   fn test_roots() {
     let mut test_proc = super::Processor {
-                          stack: Vec::new(),
-                          mem_a: 0.1,
-                          mem_b: 0.2,
-                          mem_c: 0.3,
-                          ops: Vec::new(),
-                          fns: Vec::new(),
-                        };
+      stack: Vec::new(),
+      mem_a: 0.1,
+      mem_b: 0.2,
+      mem_c: 0.3,
+      ops: Vec::new(),
+      fns: Vec::new(),
+      dict: HashMap::new(),
+    };
 
     test_proc.stack.push(2.0);
     test_proc.c_dup();
@@ -672,6 +696,7 @@ mod comp_tests {
       mem_c: 5.5,
       ops: Vec::new(),
       fns: Vec::new(),
+      dict: HashMap::new(),
     };
 
     test_proc.stack.push(1.0);
@@ -700,6 +725,7 @@ mod comp_tests {
       mem_c: 8.88888,
       ops: Vec::new(),
       fns: Vec::new(),
+      dict: HashMap::new(),
     };
 
     test_proc.stack.push(1.0);
@@ -743,6 +769,7 @@ mod comp_tests {
       mem_c: 0.0,
       ops: Vec::new(),
       fns: Vec::new(),
+      dict: HashMap::new(),
     };
 
     test_proc.stack.push(10.0);

c88a9439e27de75e778fd1420905aabfd3c1cfe8
Duane Edmonds <64871247+usefulmove@users.noreply.github.com>
2022-07-08 00:59:46 -0700

    updated test structure to remove unit tests from comp.rs into comp_test.rs; update manual test file structure

--- src/comp.rs
+++ src/comp.rs
@@ -294,7 +294,7 @@ impl Interpreter {
   }
 
 
-  // -- command functions ------------------------------------------------------
+  // command functions ---------------------------------------------------------
   // ---- stack manipulation ---------------------------------------------------
 
   fn c_drop(&mut self) {
@@ -371,7 +371,7 @@ impl Interpreter {
   }
 
 
-  // -- math operations --------------------------------------------------------
+  // ---- math operations ------------------------------------------------------
 
   fn c_add(&mut self) {
     Interpreter::check_stack_error(self, 2, "add");
@@ -502,7 +502,7 @@ impl Interpreter {
     Interpreter::check_stack_error(self, 1, "factorial");
 
     let end: usize = self.stack.len() - 1;
-    self.stack[end] = factorial(self.stack[end] as u64) as f64;
+    self.stack[end] = Interpreter::factorial(self.stack[end] as u64) as f64;
   }
 
   fn c_gcd(&mut self) {
@@ -510,7 +510,7 @@ impl Interpreter {
 
     let a: u64 = self.stack.pop().unwrap() as u64;
     let b: u64 = self.stack.pop().unwrap() as u64;
-    let g: f64 = gcd(a,b) as f64;
+    let g: f64 = Interpreter::gcd(a,b) as f64;
     self.stack.push(g);
   }
 
@@ -661,29 +661,28 @@ impl Interpreter {
   }
 
 
-
-}
-
-
-// -- support functions --------------------------------------------------------
-
-// factorial
-fn factorial(n: u64) -> u64 {
-  if n < 2 {
-    1
-  } else {
-    n * factorial(n-1)
+  // support functions ---------------------------------------------------------
+  
+  // factorial
+  fn factorial(n: u64) -> u64 {
+    if n < 2 {
+      1
+    } else {
+      n * Interpreter::factorial(n-1)
+    }
   }
+  
+  // greatest common divisor
+  fn gcd(a: u64, b: u64) -> u64 {
+    if b != 0 {
+      Interpreter::gcd(b, a % b)
+    } else {
+      a
+    }
+  }
+
 }
 
-// greatest common divisor
-fn gcd(a: u64, b: u64) -> u64 {
-  if b != 0 {
-    gcd(b, a % b)
-  } else {
-    a
-  }
-}
 
 
 // -- mona ---------------------------------------------------------------------
@@ -735,180 +734,7 @@ const MONA: &str = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>''''''<!!!!!!!!!!!!!!
        !!!!!!''       ,d$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$      allen mullen";
 
 
-// -----------------------------------------------------------------------------
-// -- unit and regression tests ------------------------------------------------
 
 #[cfg(test)]
-
-mod comp_tests {
-
-  #[test]
-  fn test_core() {
-    let mut test_cinter = super::Interpreter::new();
-
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-
-    test_cinter.c_rot();
-    test_cinter.c_rot();
-    test_cinter.c_roll();
-    test_cinter.c_roll();
-
-    test_cinter.c_dtor();
-    test_cinter.c_cos();
-    test_cinter.c_acos();
-    test_cinter.c_sin();
-    test_cinter.c_asin();
-    test_cinter.c_tan();
-    test_cinter.c_atan();
-    test_cinter.c_rtod();
-    test_cinter.c_round();
-    test_cinter.c_roll();
-    test_cinter.c_roll();
-    test_cinter.c_roll();
-    test_cinter.c_roll();
-    test_cinter.c_dup();
-    test_cinter.c_drop();
-    test_cinter.c_swap();
-    test_cinter.c_swap();
-    test_cinter.c_add();
-    test_cinter.c_sub();
-    test_cinter.c_div();
-
-    test_cinter.stack.push(10.0);
-    test_cinter.c_log2();
-    test_cinter.stack.push(10.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.c_logn();
-    test_cinter.c_sub();
-    test_cinter.c_round();
-    test_cinter.c_add();
-
-    assert!(test_cinter.stack.pop().unwrap() == -0.2);
-  }
-
-  #[test]
-  fn test_support() {
-    assert!(super::gcd(55, 10) == 5);
-    assert!(super::factorial(10) == 3628800);
-  }
-
-  #[test]
-  fn test_roots() {
-    let mut test_cinter = super::Interpreter::new();
-
-    test_cinter.stack.push(2.0);
-    test_cinter.c_dup();
-    test_cinter.c_sqrt();
-    test_cinter.c_swap();
-    test_cinter.stack.push(32.0);
-    test_cinter.c_exp();
-    test_cinter.stack.push(32.0 * 2.0);
-    test_cinter.c_throot();
-
-    assert!(test_cinter.stack.pop().unwrap() == test_cinter.stack.pop().unwrap());
-
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(-2.0);
-    test_cinter.c_chs();
-    test_cinter.c_chs();
-    test_cinter.c_pi();
-    test_cinter.c_mult();
-    test_cinter.c_pi();
-    test_cinter.stack.push(2.0);
-    test_cinter.c_exp();
-    test_cinter.stack.push(1.0);
-    test_cinter.c_add();
-    test_cinter.c_proot();
-    test_cinter.c_add_all();
-    test_cinter.stack.push(2.0);
-    test_cinter.c_div();
-    test_cinter.c_pi();
-
-    assert!(test_cinter.stack.pop().unwrap() == test_cinter.stack.pop().unwrap());
-  }
-
-  #[test]
-  #[should_panic]
-  fn test_cls() {
-    let mut test_cinter = super::Interpreter::new();
-
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.c_cls();
-
-    assert!(test_cinter.stack.pop().unwrap() == 0.0);
-  }
-
-  #[test]
-  fn test_mem() {
-    let mut test_cinter = super::Interpreter::new();
-
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.stack.push(1.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.stack.push(3.0);
-    test_cinter.stack.push(4.0);
-    test_cinter.c_chs();
-    test_cinter.c_abs();
-    test_cinter.c_inv();
-    test_cinter.c_inv();
-    test_cinter.c_pi();
-    test_cinter.c_euler();
-    test_cinter.stack.push(0.0);
-    test_cinter.c_store_b(); // 0
-    test_cinter.c_store_a(); // e
-    test_cinter.c_store_c(); // pi
-    test_cinter.c_cls();
-    test_cinter.c_push_b(); // 0
-    test_cinter.c_push_c(); // pi
-    test_cinter.c_add();
-    test_cinter.c_push_a(); // e
-    test_cinter.c_add();
-
-    assert!(test_cinter.stack.pop().unwrap() == std::f64::consts::PI + std::f64::consts::E);
-  }
-
-  #[test]
-  fn test_cmp() {
-    let mut test_cinter = super::Interpreter::new();
-
-    test_cinter.stack.push(10.0);
-    test_cinter.c_log10();
-    test_cinter.c_euler();
-    test_cinter.c_ln();
-    test_cinter.stack.push(105.0);
-    test_cinter.stack.push(2.0);
-    test_cinter.c_mod();
-    test_cinter.stack.push(3049.0);
-    test_cinter.stack.push(1009.0);
-    test_cinter.c_gcd();
-    test_cinter.c_mult_all();
-
-    assert!(test_cinter.stack.pop().unwrap() == 1.0);
-
-    test_cinter.stack.push(20.0);
-    test_cinter.c_fact();
-
-    assert!(test_cinter.stack.pop().unwrap() == 2432902008176640000.0);
-  }
-}
+#[path = "./comp_test.rs"]
+mod comp_test;

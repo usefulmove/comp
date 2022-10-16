d4491b1d6628222f937d806dd1165b28e562572d
Duane Edmonds <duane.edmonds@gmail.com>
2022-10-16 12:28:27 -0700

    (automated gcp push)

--- src/comp.rs
+++ src/comp.rs
@@ -1824,33 +1824,33 @@ impl Interpreter {
     pub fn gcd(mut u: u64, mut v: u64) -> u64 { // binary algorithm
         use std::cmp::min;
         use std::mem::swap;
-    
+
         // base cases: gcd(n, 0) = gcd(0, n) = n
         if u == 0 {return v}
         if v == 0 {return u}
-    
+
         // using identities 2 and 3:
         // gcd(2ⁱ u, 2ʲ v) = 2ᵏ gcd(u, v) with u, v odd and k = min(i, j)
         // 2ᵏ is the greatest power of two that divides both u and v
         let i = u.trailing_zeros();  u >>= i;
         let j = v.trailing_zeros();  v >>= j;
         let k = min(i, j);
-    
+
         loop {
             // u and v are odd at the start of the loop
-    
+
             // swap if necessary so u <= v
             if u > v {swap(&mut u, &mut v)}
-    
+
             // using identity 4 (gcd(u, v) = gcd(|v-u|, min(u, v))
             v -= u;
             // v is now even, but u is unchanged (and odd)
-    
+
             // identity 1: gcd(u, 0) = u
             // shift by k is necessary to add back the 2ᵏ factor that was
             // removed before the loop
             if v == 0 {return u << k}
-    
+
             // identity 3: gcd(u, 2ʲ v) = gcd(u, v) (u is known to be odd)
             v >>= v.trailing_zeros();
             // v is now odd again

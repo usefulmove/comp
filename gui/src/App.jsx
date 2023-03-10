8ddac3c42b2cf3cde74607c6bfdc0fa7a1f0e5a0
Duane Edmonds <duane.edmonds@gmail.com>
2023-03-09 22:27:23 -0800

    (automated gcp push)

--- gui/src/App.jsx
+++ gui/src/App.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, TextField } from "@mui/material";
+import { Grid, Typography, TextField, Button } from "@mui/material";
 import "./App.css";
 
 function App() {
@@ -6,23 +6,42 @@ function App() {
     <Grid container padding={4} spacing={3}>
       <Grid item xs={12}>
         <Typography variant="h5" className="title">
-          comp
+          Corbin
         </Typography>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} container>
         <TextField
           id="expression"
           label="expression"
           variant="outlined"
           color="primary"
-          sx={{ input: { color: "white" } }}
+          sx={{
+            input: { color: "white", fontFamily: "Monospace" },
+            width: "100%",
+          }}
+          focused
         />
       </Grid>
       <Grid item xs={12}>
-        <Typography sx={{ fontFamily: "Monospace" }}>
-          0.618033
+        <Typography
+          variant="h6"
+          color="#ffffff"
+          sx={{ fontFamily: "Monospace" }}
+          align="left"
+        >
+          0.6180340
+        </Typography>
+        <Typography
+          variant="h6"
+          color="#0080ff"
+          sx={{ fontFamily: "Monospace" }}
+          align="left"
+        >
+          1.6180340
           <br />
-          1.618033
+          1.2345678
+          <br />
+          512
         </Typography>
       </Grid>
     </Grid>
